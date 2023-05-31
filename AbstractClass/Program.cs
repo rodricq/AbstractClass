5017242c3e823859f6d70e253891fc2d07e6b6d4
beverley-dick <beverleydick11@gmail.com>
2023-05-31 10:22:16 +0300

    Add project files.

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class Language
+    {
+        //non-abstract method
+        public void display()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+    }
+    //inheriting from abstract class 
+    class Program : Language
+    {
+        static void Main(string [] args)
+        {
+            //object of Program class
+            Program obj = new Program();
+
+            //access method of an abstract class
+            obj.display();
+
+            Console.ReadLine();
+
+        }
+    }
+}
